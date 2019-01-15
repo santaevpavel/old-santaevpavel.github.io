3da30e3fab5d57c4302bd4da8cad1b35c86c4b45
santaev <santaevp@gmail.com>
2019-01-15 21:15:17 +0700

    change font

--- precache-manifest.992c3657a9694f67a9163787c8145e6b.js
+++ precache-manifest.992c3657a9694f67a9163787c8145e6b.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "759f07fe79cf5343b602",
-    "url": "/static/js/main.759f07fe.chunk.js"
+    "revision": "46082603eef7d36fdf4f",
+    "url": "/static/js/main.46082603.chunk.js"
   },
   {
     "revision": "10a1d9f093ce60a9f174",
     "url": "/static/js/1.10a1d9f0.chunk.js"
   },
   {
-    "revision": "759f07fe79cf5343b602",
-    "url": "/static/css/main.7699fc94.chunk.css"
+    "revision": "46082603eef7d36fdf4f",
+    "url": "/static/css/main.099c86e9.chunk.css"
   },
   {
     "revision": "10a1d9f093ce60a9f174",
     "url": "/static/css/1.9a602281.chunk.css"
   },
   {
-    "revision": "ad584a108ce4806da134eae92596c748",
+    "revision": "ec4374278b3d5a3007b98a70ebefe34d",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c7fdd64b1909beb5e21bacaf55dafda2.js"
+  "/precache-manifest.992c3657a9694f67a9163787c8145e6b.js"
 );
 
 workbox.clientsClaim();
