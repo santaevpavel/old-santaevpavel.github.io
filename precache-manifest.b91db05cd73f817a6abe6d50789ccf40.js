0c1549db2f1afcbb77e4d90e5a21c8f21b98565c
santaev <santaevp@gmail.com>
2019-01-16 23:33:17 +0700

    changed fond and styles

---
+++ precache-manifest.b91db05cd73f817a6abe6d50789ccf40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "428faf8ccf2e28fddc68",
+    "url": "/static/js/main.428faf8c.chunk.js"
+  },
+  {
+    "revision": "076d4d68d198f6321300",
+    "url": "/static/js/1.076d4d68.chunk.js"
+  },
+  {
+    "revision": "428faf8ccf2e28fddc68",
+    "url": "/static/css/main.17f67cb2.chunk.css"
+  },
+  {
+    "revision": "076d4d68d198f6321300",
+    "url": "/static/css/1.d103fd41.chunk.css"
+  },
+  {
+    "revision": "b529040832be9d12d2b39e82f8d4a934",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.992c3657a9694f67a9163787c8145e6b.js"
+  "/precache-manifest.b91db05cd73f817a6abe6d50789ccf40.js"
 );
 
 workbox.clientsClaim();
