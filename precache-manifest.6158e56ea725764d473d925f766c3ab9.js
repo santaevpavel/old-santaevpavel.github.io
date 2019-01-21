febb2ab48f21dee4e68b7707f9f91fb2df66d2d4
santaev <santaevp@gmail.com>
2019-01-21 23:17:35 +0700

    add yandex metrika

---
+++ precache-manifest.6158e56ea725764d473d925f766c3ab9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1bb569bb84fbe4cebf36",
+    "url": "/static/js/main.1bb569bb.chunk.js"
+  },
+  {
+    "revision": "5684c3c465c18893bad7",
+    "url": "/static/js/1.5684c3c4.chunk.js"
+  },
+  {
+    "revision": "1bb569bb84fbe4cebf36",
+    "url": "/static/css/main.d9fc1ed6.chunk.css"
+  },
+  {
+    "revision": "5684c3c465c18893bad7",
+    "url": "/static/css/1.3d63894a.chunk.css"
+  },
+  {
+    "revision": "1666fbf117b0906a641e174f71a40d81",
+    "url": "/index.html"
+  }
+];
